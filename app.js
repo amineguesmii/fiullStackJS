447f90b0bf804e5f9a1554024aeee2215fd30ebd
amineguesmii <108300919+amineguesmii@users.noreply.github.com>
2022-08-14 14:13:16 +0100

    clean project

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 
 var config=require('./db/config.json');
 
@@ -15,7 +13,6 @@ app.use(bodyparser.urlencoded({
   extended: true
 }));
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
@@ -26,34 +23,24 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-//import database 
 var mongoose = require('mongoose');
-//mongo config 
 const connect = mongoose.connect(
 config.mongo.uri, 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,7 @@
 import "tailwindcss/dist/base.css";
 import "styles/globalStyles.css";
 import React from "react";
-import { css } from "styled-components/macro"; //eslint-disable-line
-
-
-
+import { css } from "styled-components/macro"; 
 import ComponentRenderer from "ComponentRenderer.js";
 
 
